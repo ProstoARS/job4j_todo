9fea2b6df8ef45ae2e1bdd9c010cb078c5fbdde5
ProstoARS <arsudakov@gmail.com>
2022-12-23 21:56:17 +0300

    добавление логгера

--- src/main/java/ru/job4j/todo/repository/TaskRepository.java
+++ src/main/java/ru/job4j/todo/repository/TaskRepository.java
@@ -3,12 +3,16 @@ package ru.job4j.todo.repository;
 import lombok.AllArgsConstructor;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.Task;
 
 @Repository
 @AllArgsConstructor
 public class TaskRepository {
+
+    public static final Logger log = LoggerFactory.getLogger(TaskRepository.class.getName());
 
     private final SessionFactory sf;
 
@@ -18,9 +22,11 @@ public class TaskRepository {
             session.beginTransaction();
             session.save(task);
             session.getTransaction().commit();
+            log.info("Задача добавлена {}", task);
             session.close();
         } catch (Exception e) {
             session.getTransaction().rollback();
+            log.error("Объект {} не был добавлен", task, e);
         }
         return task;
     }
