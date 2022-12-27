d81da86c269121f0b2e2b161572cf22081d04196
ProstoARS <arsudakov@gmail.com>
2022-12-27 10:00:37 +0300

    remove .idea from repository

---
+++ src/test/java/ru/job4j/todo/repository/TaskRepositoryTest.java
@@ -0,0 +1,14 @@
+package ru.job4j.todo.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class TaskRepositoryTest {
+
+    @Test
+    public void whenAddTask() {
+
+    }
+
+}
