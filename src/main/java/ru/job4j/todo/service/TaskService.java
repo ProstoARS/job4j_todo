09aacefabd993d404fc8200e5eb260f707761d1a
ProstoARS <arsudakov@gmail.com>
2022-12-27 17:28:55 +0300

    Сервис

---
+++ src/main/java/ru/job4j/todo/service/TaskService.java
@@ -0,0 +1,35 @@
+package ru.job4j.todo.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.job4j.todo.model.Task;
+import ru.job4j.todo.repository.TaskRepository;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public Task addTask(Task task) {
+        return taskRepository.addTask(task);
+    }
+
+    public Task upgradeTask(Task task) {
+        return taskRepository.upgradeTask(task);
+    }
+
+    public Boolean deleteTask(Task task) {
+        return taskRepository.deleteTask(task);
+    }
+
+    public List<Task> findAll() {
+        return taskRepository.findAll();
+    }
+
+    public List<Task> findConditionTasks(Boolean check) {
+        return taskRepository.findConditionTasks(check);
+    }
+}
