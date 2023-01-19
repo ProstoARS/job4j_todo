d7a1e2365208b15a63114e730fab4ee91afb5c61
ProstoARS <arsudakov@gmail.com>
2023-01-19 13:50:33 +0300

    Изменение работы репозитория юзеров через паттерн "команда"

--- src/main/java/ru/job4j/todo/repository/UserRepository.java
+++ src/main/java/ru/job4j/todo/repository/UserRepository.java
@@ -3,11 +3,10 @@ package ru.job4j.todo.repository;
 import lombok.AllArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.User;
 
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
@@ -15,38 +14,23 @@ import java.util.Optional;
 public class UserRepository {
 
     private static final Logger LOG = LogManager.getLogger(UserRepository.class);
+    private static final String FIND_BY_LOGIN_AND_PASSWORD = """
+            FROM User
+            WHERE login = :tLogin
+            AND password = :tPassword
+            """;
 
-    private final SessionFactory sf;
+    private final CrudRepository crudRepository;
 
     public Optional<User> addUser(User user) {
-        Session session = sf.openSession();
-        Optional<User> rsl = Optional.empty();
-        try {
-            session.beginTransaction();
-            session.save(user);
-            session.getTransaction().commit();
-            LOG.info("Пользователь: {} был добавлен в сессии: {}", user, session);
-            session.close();
-            rsl = Optional.of(user);
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-            LOG.error("Ошибка добавления пользователя: {} в сессии: {}", user, session);
-        }
-        return rsl;
+        crudRepository.run(session -> session.persist(user));
+        return Optional.of(user);
     }
 
     public Optional<User> findByLoginAndPassword(String login, String password) {
-        Session session = sf.openSession();
-        Optional<User> rsl = Optional.empty();
-        try {
-            rsl = session.createQuery("FROM User WHERE login = :tLogin and password = :tPassword", User.class)
-                    .setParameter("tLogin", login)
-                    .setParameter("tPassword", password)
-                    .uniqueResultOptional();
-            LOG.info("Пользователь: {} нашелся по логину: {} и паролю: {}", rsl.get(), login, password);
-        } catch (Exception e) {
-            LOG.error("Пользователь с логином: {} и паролем: {} не найден", login, password);
-        }
-        return rsl;
+        return crudRepository.optional(FIND_BY_LOGIN_AND_PASSWORD, User.class, Map.of(
+                "tLogin", login,
+                "tPassword", password
+        ));
     }
 }
